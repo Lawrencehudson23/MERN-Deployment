de57f576b2832e5d6c5732ae48427166f7966f35
shijie <lawrencehudson23@gmail.com>
2020-03-24 19:31:47 -0700

    initial commit

---
+++ client/build/precache-manifest.869702808c84f78ea969cf0e6da122fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374c3c6ba324755ad35c1fbd4bfba5a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa44da85724b8fe3be34",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2c4e2525c979049e41d",
+    "url": "/static/js/2.4c7aee3c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4c7aee3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa44da85724b8fe3be34",
+    "url": "/static/js/main.3f80f91b.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
